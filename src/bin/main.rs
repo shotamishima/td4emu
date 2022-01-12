b1efb2197953444f56aee08ad930468f5d47f586
shotamishima <sm4mma@gmail.com>
2022-01-12 15:48:02 +0000

    Fixed some bugs

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,12 +1,14 @@
 use std::fs::File;
-use std::io::{BufRead, BufReader, Read};
+use std::io::{BufRead, BufReader};
 use td4emu::emulator::CpuEmulator;
 use td4emu::port::Port;
 use td4emu::register::Register;
 use td4emu::rom::Rom;
+use td4emu::compiler::Compiler;
+use td4emu::parser::Parser;
 
 fn main() {
-    let mut f = BufReader::new(File::open("example/simple_calc.sasm").expect("file not found"));
+    let f = BufReader::new(File::open("example/simple_calc.sasm").expect("file not found"));
     let ops = f.lines().map(|line| line.unwrap()).collect::<Vec<String>>();
 
     let mut source = Vec::new();
@@ -18,12 +20,24 @@ fn main() {
         }
     }
 
-    let rom = Rom::new(vec![0b00110001, 0b00000001, 0b01000000, 0b10010000]);
+    let mut parser = Parser::new(source);
+    let tokens = match parser.parse() {
+        Ok(tokens) => tokens,
+        Err(err) => panic!("{:?}", err),
+    };
+
+    let compiler = Compiler::new();
+    let program = match compiler.compile(tokens) {
+        Ok(program) => program,
+        Err(err) => panic!("{:?}", err),
+    };
+
+    let rom = Rom::new(program);
     let register = Register::new();
     let port = Port::new(0b0000, 0b0000);
     let emulator = CpuEmulator::with(register, port, rom);
     match emulator.exec() {
         Ok(_) => emulator.out(),
-        Err(err) => eprintln!("{:?}", err),
+        Err(err) => panic!("{:?}", err),
     }
 }
